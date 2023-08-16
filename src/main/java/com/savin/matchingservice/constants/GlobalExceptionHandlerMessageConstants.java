fa88ed84e73b3c8e76aafc1c5dbd35c63e7d6790
savin000 <58307835+savin000@users.noreply.github.com>
2023-08-16 21:46:45 +0300

    add constants classes

---
+++ src/main/java/com/savin/matchingservice/constants/GlobalExceptionHandlerMessageConstants.java
@@ -0,0 +1,6 @@
+package com.savin.matchingservice.constants;
+
+public class GlobalExceptionHandlerMessageConstants {
+    public static final String BASE_MESSAGE = "Unexpected exception occurred. ";
+    public static final String ENTITY_NOT_FOUND_MESSAGE = "Failed to find entity. ";
+}
